24f1671c449682c96e385143188c3cbecae5d11e
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2024-07-24 16:59:58 +0800

    配置主題色

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import daisyui from "daisyui";
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +8,68 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        customGrayLight: "#F0F0F0",
+        customGray: "#808080",
+        customBlack: "#000000",
+        customWhite: "#FFFFFF",
+        customRed: "#E6553B",
+        primary: "#7000FF",
+        secondary: "#11002D",
+        accent: "#6E01F8",
+        neutral: "#1F0F39",
+        "base-100": "#FFFFFF",
+        info: "#E9E2F3",
+        success: "#380C7E",
+        warning: "#28085",
+        error: "#000000",
       },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      "light",
+      {
+        mytheme: {
+          primary: "#7000FF",
+          secondary: "#11002D",
+          accent: "#6E01F8",
+          neutral: "#1F0F39",
+          "base-100": "#FFFFFF",
+          info: "#E9E2F3",
+          success: "#380C7E",
+          warning: "#28085",
+          error: "#000000",
+        },
+      },
+    ],
+  },
+  // daisyui: {
+  //   themes: [
+  //     ["light"],
+  //     {
+  //       mytheme: {
+  //         primary: "#7000FF",
+  //         secondary: "#11002D",
+  //         accent: "#6E01F8",
+  //         neutral: "#1F0F39",
+  //         "base-100": "#FFFFFF",
+  //         info: "#E9E2F3",
+  //         success: "#380C7E",
+  //         warning: "#28085",
+  //         error: "#000000",
+  //       },
+  //     },
+  //     // other themes...
+  //   ],
+
+  //   darkMode: false,
+  // },
+  plugins: [daisyui],
 };
 export default config;
